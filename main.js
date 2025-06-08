a111c9f43962a52033a52283c6da5a198d2b9fb9
Sung-Hoon24 <Heejung240000@gmail.com>
2025-06-08 19:19:26 +0900

    Update main.js

--- main.js
+++ main.js
@@ -83,8 +83,8 @@ const rtan = {
 /** end of 1-1 르탄이 그리기 */
 
 /** 2-1 장애물 설정 */
-const OBSTACLE_WIDTH = 50; // 장애물 너비
-const OBSTACLE_HEIGHT = 50; // 장애물 높이
+const OBSTACLE_WIDTH = 70; // 장애물 너비
+const OBSTACLE_HEIGHT = 70; // 장애물 높이
 const OBSTACLE_FREQUENCY = 110; // 장애물 생성 빈도
 const OBSTACLE_SPEED = 5; // 장애물 이동 속도
 
